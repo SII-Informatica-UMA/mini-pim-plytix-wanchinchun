098623013c971c0ab4aee25eccec153426f46cb8
Yisus <bsantossjesus@gmail.com>
2025-03-22 11:57:49 +0100

    Initial project setup

---
+++ Backend/src/main/java/com/uma/wanchinchun/WanchinchunApplication.java
@@ -0,0 +1,13 @@
+package com.uma.wanchinchun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanchinchunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WanchinchunApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/uma/wanchinchun/WanchinchunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uma.wanchinchun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanchinchunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
