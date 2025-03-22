f9c90812c1163df2b5d2354dc9f5b5092842ef93
Yisus <bsantossjesus@gmail.com>
2025-03-22 17:17:14 +0100

    CommandLineRunner empty implementation

--- Backend/src/main/java/com/uma/wanchinchun/WanchinchunApplication.java
+++ Backend/src/main/java/com/uma/wanchinchun/WanchinchunApplication.java
@@ -1,13 +1,18 @@
 package com.uma.wanchinchun;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WanchinchunApplication {
+public class WanchinchunApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WanchinchunApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
