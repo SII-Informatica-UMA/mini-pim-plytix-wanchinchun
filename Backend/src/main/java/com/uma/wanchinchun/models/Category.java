cabc38eed5dbc3b3dfb2c3484df858b044eea6df
Yisus <bsantossjesus@gmail.com>
2025-04-21 17:51:13 +0200

    Fix in models to better meet requirements

--- Backend/src/main/java/com/uma/wanchinchun/models/Category.java
+++ Backend/src/main/java/com/uma/wanchinchun/models/Category.java
@@ -12,6 +12,7 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(nullable = false)
     private String nombre;
     @ManyToMany(mappedBy = "categorias")
     private Set<Product> productos = new HashSet<>();
