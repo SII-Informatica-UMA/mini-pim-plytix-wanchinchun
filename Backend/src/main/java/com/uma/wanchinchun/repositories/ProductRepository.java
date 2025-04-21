5aa0b44b73017f746ba257fd6f9dbdcaa5599658
Yisus <bsantossjesus@gmail.com>
2025-04-21 17:56:59 +0200

    Method added to Product Repository

--- Backend/src/main/java/com/uma/wanchinchun/repositories/ProductRepository.java
+++ Backend/src/main/java/com/uma/wanchinchun/repositories/ProductRepository.java
@@ -2,6 +2,8 @@ package com.uma.wanchinchun.repositories;
 
 import com.uma.wanchinchun.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +12,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findByGtin(String gtin);
     List<Product> findByCategoriasId(Long categoriaId);
     List<Product> findByCategoriasNombre(String nombreCategoria);
+    @Query("SELECT COUNT(prods) FROM Productos p WHERE p.idCuenta = :idCuenta")
+    Integer countProductsOwnedByAccount(@Param("idCuenta") Long idCuenta);
 }
